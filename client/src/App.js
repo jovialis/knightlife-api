0da0732dcfe03164b172f3e4aef16a7da52fe6d4
Dylan Hanson <cdh.create@gmail.com>
2018-07-06 00:37:58 -0400

    Deleted extra imports. Woopsie!

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,6 @@ import React, {Component} from 'react';
 // Styles
 import './App.css';
 
-//Import
-import Header from './header/Header'
-import FeedSection from './section/FeedSection'
-
 class App extends Component {
 	render() {
 		return (
