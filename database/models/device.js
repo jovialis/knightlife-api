108a83d73be6636f82b6812cec6cf5e4e3de2801
Dylan Hanson <cdh.create@gmail.com>
2018-08-13 22:13:49 -0400

    Remove _id so it isn't required for new Device object.

--- database/models/device.js
+++ database/models/device.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-	_id: {
-		type: mongoose.Schema.Types.ObjectId,
-		select: false
-	},
 	token: String
 }, {
 	collection: "devices",
