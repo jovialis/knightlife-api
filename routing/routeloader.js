7ab5a0c124d65476aab2852751723e1e9a2fbd9a
Dylan Hanson <cdh.create@gmail.com>
2018-08-16 22:49:56 -0400

    Only allow pushing if you're logged in

--- routing/routeloader.js
+++ routing/routeloader.js
@@ -23,18 +23,27 @@ module.exports = function (app) {
     app.use(bodyParser.json());
     
     // Register POST
-    app.post('/api/push/refresh/events', require('./routes/push/refresh/events'));
-    app.post('/api/push/refresh/lunch', require('./routes/push/refresh/lunch'));
-    app.post('/api/push/refresh/schedule', require('./routes/push/refresh/schedule'));
-    app.post('/api/push/message', require('./routes/push/message'));
+    app.post('/api/push/refresh/events', loggedIn, require('./routes/push/refresh/events'));
+    app.post('/api/push/refresh/lunch', loggedIn, require('./routes/push/refresh/lunch'));
+    app.post('/api/push/refresh/schedule', loggedIn, require('./routes/push/refresh/schedule'));
+    app.post('/api/push/message', loggedIn, require('./routes/push/message'));
     
     app.get('/api/device/register', require('./routes/device/registration'));
     
+    // Setup Google Auth via passport
     app.use(passport.initialize());
     
-    app.get('/login/auth/google', passport.authenticate('google', { scope: ['profile'] }))
+    app.get('/login/auth/google', passport.authenticate('google', { scope: ['profile'] }));
     app.get('/login/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function(req, res) {
         res.redirect('/');
     });
     
 };
+
+function loggedIn(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
