21ae48e6f52e8a4163be191a12b280db3f9545c3
Dylan Hanson <cdh.create@gmail.com>
2018-08-27 13:19:50 -0400

    Add Help page.

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import './index.css';
 
 // Imports
 import Home from './pages/Home';
+import Help from './pages/Help';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 
@@ -14,6 +15,7 @@ ReactDOM.render(
   <Router>
       <div>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/help" component={ Help } />
         <Route exact path="/login" component={ Login } />
         <Route exact path="/dashboard" component={ Dashboard } />
       </div>
---
+++ client/src/pages/Help.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// Styles
+import './help/Help.css';
+
+class Help extends Component {
+	render() {
+		return (
+			<div className="help">
+                Help
+			</div>
+		);
+	}
+}
+
+export default Help;
