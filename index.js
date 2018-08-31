3a0ca7e18590251c4d37e364fadf5b5666488b58
Dylan Hanson <cdh.create@gmail.com>
2018-08-31 12:45:33 -0400

    Reinstall cors

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ const path = require('path');
 const enforce = require('express-sslify');
 const http = require('http');
 
+const cors = require('cors');
+
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(express.static(path.join(__dirname, 'client/build')));
