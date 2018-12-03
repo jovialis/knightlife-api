8b5e091ded4469457b9e2aa43e311ee41b9d1610
Dylan Hanson <cdh.create@gmail.com>
2018-12-02 20:05:06 -0500

    Adjusted styles, added GET to request fielder.

--- app/routing/routes/portal/auth/google/post-retrieve-google-redirect.js
+++ app/routing/routes/portal/auth/google/post-retrieve-google-redirect.js
@@ -1,5 +1,5 @@
 module.exports.register = (app) => {
-    app.post('/login/google', (req, res) => {
+    app.get('/login/google', (req, res) => {
         const url = require(`${ __basedir }/app/google/google-loader`).urlGoogle();
         res.redirect(url);
     });
