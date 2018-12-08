f86eaf9256b0c4a009c66abdb7c9bd82cd159a29
Dylan Hanson <cdh.create@gmail.com>
2018-12-07 22:34:56 -0500

    Wrap around for Sunday

--- app/interface/day/complications/complication-schedule.js
+++ app/interface/day/complications/complication-schedule.js
@@ -7,7 +7,12 @@ module.exports.create = (day) => {
         const Schedule = mongoose.model('Schedule');
 
         const date = day.date;
-        const dayIndex = ((date.getDay() - 1) % 7); // So monday is 0, sunday is 6
+        const dayIndex = date.getDay() - 1; // So monday is 0, sunday is 6
+        
+        // Sunday wrap around, because mod doesn't work in JS.
+        if (dayIndex === -1) {
+            dayIndex = 6;
+        }
 
         const template = require('./schedule-template.json');
         const dayBlocks = template.days[dayIndex].blocks;
