d1e61953a8ada221f5de330ec784c00553c2293c
Dylan Hanson <cdh.create@gmail.com>
2018-12-08 11:43:12 -0500

    Remove hasOwnProperty because it doesn't exist?!

--- app/utils/sanitize-keys.js
+++ app/utils/sanitize-keys.js
@@ -4,12 +4,10 @@ module.exports.sanitize = (object, keys) => {
 
 function sanitizeKeys(object, keys) {
     for (let key in object) {
-        if (object.hasOwnProperty(key)) {
-            if (typeof object[key] == "object") {
-                sanitizeKeys(object[key], keys);
-            } else if (keys.includes(key)) {
-                delete object[key];
-            }
+        if (typeof object[key] == "object") {
+            sanitizeKeys(object[key], keys);
+        } else if (keys.includes(key)) {
+            delete object[key];
         }
     }
     return object;
