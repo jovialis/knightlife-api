47489146d5fab3e928a3e4c68c82aa3c9cdf2e98
Dylan Hanson <cdh.create@gmail.com>
2019-05-05 20:32:55 -0400

    Rename variable

--- routes/router.js
+++ routes/router.js
@@ -30,7 +30,7 @@ module.exports.init = () => {
 
 	// ENABLE OPTION REQUESTS FOR CORS
 	// http://johnzhang.io/options-request-in-express
-	app.options("/*", function(req, res, next){
+	router.options("/*", function(req, res, next){
 		res.header('Access-Control-Allow-Origin', '*');
 		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 		res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
