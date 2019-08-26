20a221c8181b7b78408cb786b2ea7fd2f4f4300d
Elizabeth Cortell <liz@ziprecruiter.com>
2019-08-25 19:47:39 -0700

    Start up HTTPS with self-signed cert in development; otherwise trust proxy and start normal server.

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const https = require('https');
 const bodyParser = require('body-parser');
 const expressSsl = require('express-sslify');
 const Cookies = require('cookies');
+const fs = require('fs');
 
 // Instantiation
 module.exports.init = () => {
+	// The application object.
 	const router = express();
 
 	// POST middleware
@@ -13,26 +16,51 @@ module.exports.init = () => {
 
 	// Instantiate cookies object in request
 	router.use(Cookies.express([ process.env.COOKIE_SECRET ]));
-
-	if (process.env.NODE_ENV === 'production') {
-		// Redirect to HTTPS in production
-		router.use(expressSsl.HTTPS({trustProtoHeader: true}));
-
-		// Trust Proxy in production
-		router.enable('trust proxy');
-	}
-
+	router.use(expressSsl.HTTPS({trustProtoHeader: true}));
 	registerRoutes(router);
+
+	// Handle errors with express
+	router.use((err, req, res, next) => {
+		console.log(err);
+
+		// If detailed error available
+		if (err instanceof DetailedError) {
+			// Send JSON response
+			res.status(err.errorCode).send({
+				error: err.errorId,
+				errorMessage: err.errorMessage
+			});
+		} else {
+			res.status(500).send({
+				error: 'err_server',
+				errorMessage: 'An internal error occurred. Please contact an administrator.'
+			});
+		}
+	});
 
 	router.all('*', (req, res) => {
 		res.status(404).send("Not Found")
 	});
 
-	// Grab port and start listening
 	const port = process.env.PORT || 5000;
-	router.listen(port, () => {
-		console.log("Express is now listening on port: " + port);
-	});
+
+	// NODE_ENV is set in the docker-compose.yml
+	if (process.env.NODE_ENV === 'development') {
+		// For these certificates to be trusted, rootCA.cer must be installed
+		// on the simulator. See docker/genca/Dockerfile.
+		https.createServer({
+			key:  fs.readFileSync('/home/node/localhost.key'),
+			cert: fs.readFileSync('/home/node/localhost.cer')
+		}, router).listen(port, () => {
+			console.log("Express is now listening on port: " + port);
+		});
+	}
+	else {
+		router.enable('trust proxy');
+		router.listen(port, () => {
+			console.log("Express is now listening on port: " + port);
+		});
+	}
 };
 
 function registerRoutes(router) {
