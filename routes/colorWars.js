d6c26b74aed8d1d03e26328a0c3e0701c3b424e2
Potato <hcprice115@gmail.com>
2019-12-05 11:33:57 -0500

    make it better

--- routes/colorWars.js
+++ routes/colorWars.js
@@ -4,7 +4,7 @@ const auth = require('../middlewares/auth');
 module.exports.registerRoutes = (router) => {
 
 	// Mobile
-	router.get('/m/colorwars/:team', controller.routeGetPointsbyTeam);
+	router.get('/m/colorwars/:team', controller.routeGetPointsByTeam);
 	router.get('/m/colorwars/:badge', controller.routeGetTeamByBadge);
 
 };
